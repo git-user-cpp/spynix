88a130a4dc3d6d9a093cf4aeb2c871f25de88d12
git-user-cpp <akushyk799@gmail.com>
2024-01-05 16:10:15 +0200

    displaying rom info

--- src/rom/rom_info.h
+++ src/rom/rom_info.h
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <sys/statvfs.h>
 
+void print_rom_info() {
+    printf("\nDisk Information:\n");
+    struct statvfs fs_info;
+    if (statvfs("/", &fs_info) == 0) {
+        printf("Total Disk Space: %ld MiB\n", fs_info.f_blocks * fs_info.f_bsize
+        / 1024 / 1024);
+        printf("Free Disk Space: %ld MiB\n", fs_info.f_bfree * fs_info.f_bsize /
+        1024 / 1024);
+        printf("Used Disk Space: %ld MiB\n", (fs_info.f_blocks -
+        fs_info.f_bfree) * fs_info.f_bsize / 1024 / 1024);
+    }
+}
