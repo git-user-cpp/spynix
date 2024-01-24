1c7d6c9223853f2282a2b060c6e0c7daeb2efc4b
git-user-cpp <akushyk799@gmail.com>
2024-01-24 16:09:42 +0200

    provided colorful output for info options

--- src/hlp/hlp_info.c
+++ src/hlp/hlp_info.c
@@ -12,68 +12,69 @@
 
 void print_banner(void)
 {
-        printf("\n \
+        printf("\033[34m\n \
          ____                    _\n \
         / ___| _ __  _   _ _ __ (_)_  __\n \
         \\___ \\| '_ \\| | | | '_ \\| \\ \\/ /\n \
          ___) | |_) | |_| | | | | |>  <\n \
         |____/| .__/ \\__, |_| |_|_/_/\\_\\\n \
-              |_|    |___/\n\n");
+              |_|    |___/\033[0m\n\n");
 }
 
 void print_logo(void)
 {
-        printf("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNK0OkxxdddddxkkO0KNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n\
+        printf("\033[34mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNK0OkxxdddddxkkO0KNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n\
 MMMMMMMMMMMMMMMMMMMMMMMWXKOxdoc:;,,''''''''',,;:codxOKXWMMMMMMMMMMMMMMMMMMMMMMM\n\
 MMMMMMMMMMMMMMMMMMMWKOdl;,'',;:clooooddddoooollc:;''',:ldOKWMMMMMMMMMMMMMMMMMMM\n\
 MMMMMMMMMMMMMMMMN0dc,.',:loxkOOOOOOOOOOOOOOOOOOOOkxdoc;'..,cd0NMMMMMMMMMMMMMMMM\n\
-MMMMMMMMMMMMMW0d:'.':oxOOOOOkkxdollcc::::ccclodxkkOOO0Okoc,..':d0WMMMMMMMMMMMMM\n");
-        printf("MMMMMMMMMMWXkl'.':okOOOOkxoc;'...''.........'...',:ldkOOO0Oxl,..,lkNWMMMMMMMMMM\n\
+MMMMMMMMMMMMMW0d:'.':oxOOOOOkkxdollcc::::ccclodxkkOOO0Okoc,..':d0WMMMMMMMMMMMMM\033[0m\n");
+        printf("\033[34mMMMMMMMMMMWXkl'.':okOOOOkxoc;'...''.........'...',:ldkOOO0Oxl,..,lkNWMMMMMMMMMM\n\
 MMMMMMMMMXk:..,lxOOOOxo:'.  ......,;:lllc:;'....'. ...;lxkOOOOd:...:kNMMMMMMMMM\n\
 MMMMMMMNO:..,okOkOkd:.  .' .;cc:::;'......,:loxO0o..;.  .,lxOkOOxc...:OWMMMMMMM\n\
 MMMMMWKo'.'lkOkOkl'   .,c. ..  ..';cclodddxdooodOO;.;c..   .:dOkkOx:..'oKMMMMMM\n\
-MMMMWO;..:xOkkko'   ..,l,  .         .','..',;clx0x..cl,..   .:xOkkOd,..;OWMMMM\n");
-        printf("MMMNx'..lOkkkx;   ..';lc.                ...',;cldk; 'oc,,...  .ckkkOk:..,xNMMM\n\
+MMMMWO;..:xOkkko'   ..,l,  .         .','..',;clx0x..cl,..   .:xOkkOd,..;OWMMMM\033[0m\n");
+        printf("\033[34mMMMNx'..lOkkkx;   ..';lc.                ...',;cldk; 'oc,,...  .ckkkOk:..,xNMMM\n\
 MMNd'.'dOxkkl.  ..',;lo'                    ..',;co:. :oc;;'..   ,dkxkOl..'dNMM\n\
 MWx'.'dkxkk:. ..,::ccc,                    ...';:lkk, .:cc:::,'.  .okxxOl..'xWM\n\
 WO, .okxkk:   .',,,;;,'.              ......';:codk0xc::;;;,,,'.   .lkxxkc..,OW\n\
-K:..ckxxk:  ...,clcc:,..          ...',,;;:cclooooddxkOO00Oko:'...  .okdxx; .cK\n");
-        printf("x. ,dxdkl.  .','..      .......'''''',,,;;;:::cclllllcc:;,'..',,'.   'dxdxo. .x\n\
+K:..ckxxk:  ...,clcc:,..          ...',,;;:cclooooddxkOO00Oko:'...  .okdxx; .cK\033[0m\n");
+        printf("\033[34mx. ,dxdkl.  .','..      .......'''''',,,;;;:::cclllllcc:;,'..',,'.   'dxdxo. .x\n\
 :..cxdxd'   ..';:::;.       ..........''''...'''....     '::c:,'..    :xddx; .:\n\
 ' 'odoxl.   ...',;co'      ..,,'.           .',;'.      .co:;,''...   'odoxl. '\n\
 . ,ddox;    ...',;co, ..  .ck0K0d;.       .'lOKKOo,.  '..co:;,'....   .ldldo' .\n\
-. ;dood,    ...',;:oc.'' ':oOKXKkc;. ;l:'.,:d0KX0xc;..,.'ol:;,''...    cdloo' .\n");
-        printf(". ;dood,    ...',,;co;.. .;cloolc:,..lkk: .:cloolc:'....co:;,,'....    :dloo' .\n\
+. ;dood,    ...',;:oc.'' ':oOKXKkc;. ;l:'.,:d0KX0xc;..,.'ol:;,''...    cdloo' .\033[0m\n");
+        printf("\033[34m. ;dood,    ...',,;co;.. .;cloolc:,..lkk: .:cloolc:'....co:;,,'....    :dloo' .\n\
 . ,oold;    ...',;;:lo;   ..'''''.  .oO0l. ..'','''.  .loc:;,,'....   .cocol. .\n\
 . .lococ.   ...',;:lll;.            .o00o.......';o:  'cloc:;,'...    .llcoc. .\n\
 ; .:ocll'    ..,:c:,'',c;...         .,'..,;::cldOx'.c:'.';:c:;'..    ,lcco;  :\n\
-d. 'lc:l:.  .',,'.':oxdc'  .          .',;:::cloxk: .;oxdl;..',,'.   .cl:lc. .d\n");
-        printf("K; .:l::l,  '.. ,okdc'.     .         'cloc::clxkc.    .,lxko, ..'.  ;c::l,  :K\n\
+d. 'lc:l:.  .',,'.':oxdc'  .          .',;:::cloxk: .;oxdl;..',,'.   .cl:lc. .d\033[0m\n");
+        printf("\033[34mK; .:l::l,  '.. ,okdc'.     .         'cloc::clxkc.    .,lxko, ..'.  ;c::l,  :K\n\
 Wk' .cc;:c' ....,xx,.        ..       .,;llccldx:        .;kk;....  ,c;;c:. 'kW\n\
 MNd. .cc,;c'  .'..:l;.               ...',:cooc.        .:dl...'.  ,c;,c:. .dNM\n\
 MMNo. .::,;:;. .'..'c:.            ..,;::cc:;.         .:l,..,.  .,:,,::. .oNMM\n\
-MMMNd. .;:,'::.  .'..,:'.            ......          .'::..''.  .;;',:;. .dNMMM\n");
-        printf("MMMMNx' .,:,',:,. .....;,.                          .,:'..'.  .;;'';:'. 'kWMMMM\n\
+MMMNd. .;:,'::.  .'..,:'.            ......          .'::..''.  .;;',:;. .dNMMM\033[0m\n");
+        printf("\033[34mMMMMNx' .,:,',:,. .....;,.                          .,:'..'.  .;;'';:'. 'kWMMMM\n\
 MMMMMW0:. .;;'.,;,.    .',.                        .,,. ..  .,;,.';,. .c0WMMMMM\n\
 MMMMMMMNx,. .;;'.,;,..   .'.                      .,.    .',;'.';,. .,xNMMMMMMM\n\
 MMMMMMMMWXd,. .,,,..,;,'. ...                    ...  .',,,..,,,. .,dXMMMMMMMMM\n\
-MMMMMMMMMMWXd;. .',,'..,,,,'..                  ...'',,'..',,.. .;xXWMMMMMMMMMM\n");
-        printf("MMMMMMMMMMMMMNOl'. ..'''...',,,,''''''''''''''',,,''..''''.. .'lONMMMMMMMMMMMMM\n\
+MMMMMMMMMMWXd;. .',,'..,,,,'..                  ...'',,'..',,.. .;xXWMMMMMMMMMM\033[0m\n");
+        printf("\033[34mMMMMMMMMMMMMMNOl'. ..'''...',,,,''''''''''''''',,,''..''''.. .'lONMMMMMMMMMMMMM\n\
 MMMMMMMMMMMMMMMWXkl,. ......'''...'''''''''......''.....  .,lkXWMMMMMMMMMMMMMMM\n\
 MMMMMMMMMMMMMMMMMMWN0dc,.. ........................  ..,cd0NWMMMMMMMMMMMMMMMMMM\n\
 MMMMMMMMMMMMMMMMMMMMMMWNKOdl:,'...           ...',:ldOKNWMMMMMMMMMMMMMMMMMMMMMM\n\
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKOkdlc:::::::cldkOKNWMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n");
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKOkdlc:::::::cldkOKNWMMMMMMMMMMMMMMMMMMMMMMMMMMMM\033[0m\n");
 }
 
 void print_err_info(void)
 {
-        printf("Use spynix -h or spynix --help to display all valid options\n");
+        printf("Use spynix \033[31m-h\033[0m or spynix \033[31m--help\033[0m to display all valid options\n");
 }
 
 void print_hlp_info(void)
 {
         print_banner();
-        printf("Spynix is a commandline tool for gathering info about hardware.\n\n\
+        printf("\033[36mSpynix is a commandline tool for gathering info about \
+hardware.\033[0m\n\n\
 \t\tInfo:\n\
 \t\t\t-h or --help \t\t- show this menu\n\
 \t\t\t-v or --version \t- show version\n\
@@ -94,6 +95,6 @@ void print_ver_info(void)
 {
         print_banner();
         printf("spynix v4.0.2\n\nFor more info visit: \
-https://github.com/git-user-cpp/spynix\n");
+\033[36mhttps://github.com/git-user-cpp/spynix\033[0m\n");
 }
 
