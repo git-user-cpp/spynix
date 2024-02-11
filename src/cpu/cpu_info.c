1ac69ee57630214521218382772d929feb67acdb
git-user-cpp <akushyk799@proton.me>
2024-02-11 11:20:38 +0200

    using lscpu for showing general cpu details

--- src/cpu/cpu_info.c
+++ src/cpu/cpu_info.c
@@ -16,34 +16,19 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "cpu_info.h" 
 
 void print_cpu_info(void)
 {
-        FILE *cpu_file;
-
         printf(" ____________\n\
 |\n\
 | CPU Info:\n\
 |____________\n\
 |\n");
 
-        cpu_file = fopen("/proc/cpuinfo", "r");
-        if (cpu_file) {
-                char line[128];
-                uint8_t lines = 0;
-                
-                while (fgets(line, sizeof(line), cpu_file) && lines < 19) {
-                        printf("| %s", line);
-                        ++lines;
-                }
-
-                fclose(cpu_file);
-
-                printf("|____________\n\n");
-        } else {
-                printf("Error opening /proc/cpuinfo\n");
-        }
+        system("lscpu");
+        printf("|____________\n\n");
 }
 
 void print_full_cpu_info(void)
